dbd94199c21bb5902088e75b9e7122e15478ccb4
叶俊泽 <128799728+MasterToycode@users.noreply.github.com>
2023-07-15 00:06:08 +0800

    Add files via upload

---
+++ 动态规划/著名的活动选择问题（动态规划版本）.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int  Fun_activity(vector<int>& s, vector<int>& f, int n1, int n2, vector<vector<int>>& m, vector<vector<int>>& p)
+{
+    if (m[n1][n2] != -1)
+    {
+        return m[n1][n2];
+    }
+    else if (n1 + 1 == n2)
+    {
+        m[n1][n2] = 1;
+    }
+    else
+    {
+        int maxCount = 0;
+        for (int i = n1 + 1; i < n2; i++)
+        {
+            if (f[n1] <= s[i] && f[i] <= s[n2])  // 检查活动的兼容性
+            {
+                Fun_activity(s, f, n1, i, m, p);
+                Fun_activity(s, f, i, n2, m, p);
+                int count = m[n1][i] + m[i][n2] + 1;
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    p[n1][n2] = i; // 更新p数组，存储当前的最大兼容组合
+                }
+            }
+        }
+        m[n1][n2] = maxCount;
+        return m[n1][n2];
+    }
+}
+
+void GetMaxCompatibilities(vector<int>& s, vector<int>& f, vector<vector<int>>& p)
+{
+    int n = s.size();
+    vector<vector<int>> m(n, vector<int>(n, -1));
+    cout << "最大相互兼容活动组合的数量为：" << Fun_activity(s, f, 0, n - 1, m, p) << endl;
+}
+
+int main()
+{
+    vector<int> s = { 0, 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12 };
+    vector<int> f = { 0, 4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16 };
+    int n = s.size();
+    vector<vector<int>> p(n, vector<int>(n, -1));
+    GetMaxCompatibilities(s, f, p);
+    cout << endl;
+    return 0;
+}
